170acdc7382966c0f3ffa6dce8de76569afee5e2
Matheus Andre <tksheisters@gmail.com>
2023-05-18 17:15:22 -0300

    primeira classe Position

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Position pos = new Position(1, 2);
+		System.out.println(pos);
 
 	}
 
