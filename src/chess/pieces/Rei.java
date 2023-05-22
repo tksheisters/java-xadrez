cd88b4e33e069c57663d131d582d6e8863edb22c
Matheus Andre <tksheisters@gmail.com>
2023-05-22 18:07:55 -0300

    possivel movimento de uma peca

--- src/chess/pieces/Rei.java
+++ src/chess/pieces/Rei.java
@@ -14,4 +14,10 @@ public class Rei extends ChessPiece{
 	public String toString() {
 		return "R";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
--- src/chess/pieces/Torre.java
+++ src/chess/pieces/Torre.java
@@ -14,4 +14,10 @@ public class Torre extends ChessPiece{
 	public String toString() {
 		return "T";		
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
